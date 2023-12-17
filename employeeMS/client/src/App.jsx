c003569dceef406899809e91d8f8cdf4b9a11505
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-12-17 14:52:51 +0530

    Packages Installed
    Fontawesome and Bootstrap packages were installed.

--- employeeMS/client/src/App.jsx
+++ employeeMS/client/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Stack from 'react-bootstrap/Stack';
+import Container from 'react-bootstrap/Container';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {/* <div style={{width: 260, height: 889, background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 39}} />
+      <Card style={{ width: '18rem' }}> */}
+      <Container fluid>
+        <Card style={{ width: '18rem' }}>
+            
+            <Stack gap={3} className='text-center'>
+                <div className="rowCard"><FontAwesomeIcon icon={faHouse}/>Dashboard</div>
+                <div className="rowCard">Account</div><FontAwesomeIcon icon={faCircleUser} />
+                <div className="rowCard">Tasks</div><FontAwesomeIcon icon={faCalendarDays} />
+                <div className="rowCard">Payroll</div><FontAwesomeIcon icon={faHandHoldingDollar} />
+                <div className="rowCard">Messages</div><FontAwesomeIcon icon={faMessage} />
+                <div className="rowCard">Settings</div><FontAwesomeIcon icon={faGear} />
+                <NavDropdown.Divider />
+                <div className="rowCard">Logout</div><FontAwesomeIcon icon={faArrowRightFromBracket} />
+            </Stack>
+            <Card.Body>                
+                
+            </Card.Body>
+        </Card> 
+      </Container>
+            
+    </div>
   )
 }
 
