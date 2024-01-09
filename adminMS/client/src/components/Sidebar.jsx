1550f8ad71a8fce7dabbe23fd15e9f33742ba0f0
Teshan2000 <teshan_wijewardhana@outlook.com>
2024-01-09 13:29:06 +0530

    Admin Sidebar
    Sidebar of admin options was created.

---
+++ adminMS/client/src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightFromBracket, faMessage, faCalendarDays, faCalendarDay, faCalendarCheck, faCircleUser, faBoxArchive, faHouse, faPieChart, faRectangleList } from '@fortawesome/free-solid-svg-icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Stack from 'react-bootstrap/Stack';
+import Nav from 'react-bootstrap/Nav';
+
+function Sidebar() {
+  return (
+    <div className='sidebar mx-3'>
+      <Stack gap={2} className='justify-content-center px-5 py-3'>
+          <Nav.Link href="./pages/Home"><div className="rowCard"><FontAwesomeIcon icon={faHouse} className="px-2"/>Dashboard</div></Nav.Link>
+          <Nav.Link href="./pages/Home"><div className="rowCard"><FontAwesomeIcon icon={faCircleUser} className="px-2"/>Account</div></Nav.Link>
+          <Nav.Link href="./pages/Home"><div className="rowCard"><FontAwesomeIcon icon={faBoxArchive} className="px-2"/>Departments</div></Nav.Link>
+          <Nav.Link href="./pages/Home"><div className="rowCard"><FontAwesomeIcon icon={faCalendarCheck} className="px-2"/>Attendance</div></Nav.Link>
+          <Nav.Link href="./pages/Home"><div className="rowCard"><FontAwesomeIcon icon={faRectangleList} className="px-2"/>Requests</div></Nav.Link>
+          <Nav.Link href="./pages/Home"><div className="rowCard"><FontAwesomeIcon icon={faMoneyBill1Wave} className="px-2"/>Payments</div></Nav.Link>
+          <Nav.Link href="./pages/Home"><div className="rowCard"><FontAwesomeIcon icon={faCalendarDays} className="px-2"/>Calendar</div></Nav.Link>
+          <Nav.Link href="./pages/Home"><div className="rowCard"><FontAwesomeIcon icon={faCalendarDay} className="px-2"/>Leave Management</div></Nav.Link>                  
+          <Nav.Link href="./pages/Home"><div className="rowCard"><FontAwesomeIcon icon={faMessage} className="px-2"/>Messages</div></Nav.Link>
+          <Nav.Link href="./pages/Home"><div className="rowCard"><FontAwesomeIcon icon={faPieChart} className="px-2"/>Performance</div></Nav.Link>           
+          <Nav.Link href="./pages/Home"><div className="rowCard"><FontAwesomeIcon icon={faArrowRightFromBracket} className="px-2"/><hr/>Logout</div></Nav.Link>
+      </Stack>       
+    </div>    
+  )
+}
+
+export default Sidebar
+
+
+
