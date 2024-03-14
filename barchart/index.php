9678842d24f9d275eba2c7bd73042d50c0663f71
UviniRandinithi <90383652+UviniRandinithi@users.noreply.github.com>
2024-03-14 20:15:45 +0530

    commit 14

--- barchart/index.php
+++ barchart/index.php
@@ -42,7 +42,7 @@ foreach ($attendance_data as $dept => $counts) {
     <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
     <script>
         window.onload = function () {
-            var chart = new CanvasJS.Chart("chartContainer", {
+            var chart = new CanvasJS.Chart("barchart", {
                 animationEnabled: true,
                 theme: "light2",
                 title:{
@@ -78,6 +78,6 @@ foreach ($attendance_data as $dept => $counts) {
     </script>
 </head>
 <body>
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<div id="barchart" style="height: 370px; width: 100%;"></div>
 </body>
 </html>
